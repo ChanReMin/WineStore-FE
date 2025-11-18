f63df818f8e85410a40b4ec4e762b057864e4c43
leminh <minhletran152003@gmail.com>
2025-11-18 09:28:37 +0700

    feat: init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata, Viewport } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Inter, Roboto_Mono } from 'next/font/google';
 import './globals.css';
 import {
   absoluteUrl,
@@ -11,12 +11,12 @@ import {
 import Header from '@/components/header';
 import Footer from '@/components/Footer/Footer';
 
-const geistSans = Geist({
+const geistSans = Inter({
   variable: '--font-geist-sans',
   subsets: ['latin'],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = Roboto_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
 });
